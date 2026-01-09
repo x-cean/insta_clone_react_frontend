b2fb062f7fc9d081cc16e8f690077035c3773651
xiao <194778728+x-cean@users.noreply.github.com>
2026-01-09 18:07:48 +0100

    Updated axios baseURL to the backend address

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 // We define the base URL of our backend API.
+
+// for local development use http://localhost:3000
+
+// for deployed backend use the Render URL below
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Your Fastify backend address
+  baseURL: "https://insta-clone-backend-8x0g.onrender.com", // Your Fastify backend address
 });
 
 export { api };
